bf1334d98fa70c0561c519c0f5b82894dcb035c2
mnkillebr <killebrew.biz@gmail.com>
2025-04-12 09:20:47 -0400

    add sidebar to dash layout

---
+++ rr_frontend/app/components/app-sidebar.tsx
@@ -0,0 +1,66 @@
+
+import { Building, LayoutDashboard } from "lucide-react"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "~/components/ui/sidebar"
+import { NavUser } from "./nav-user"
+import { NavMain } from "./nav-main"
+
+const data = {
+  user: {
+    name: "shadcn",
+    email: "m@example.com",
+    avatar: "https://i.pravatar.cc/150?img=34",
+  },
+  organization: {
+    name: "Killebrew Inc",
+    logo: Building,
+    plan: "Enterprise",
+  },
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "#",
+      icon: LayoutDashboard,
+    },
+  ],
+}
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <data.organization.logo className="size-4" />
+              </div>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
+                  {data.organization.name}
+                </span>
+                <span className="truncate text-xs">{data.organization.plan}</span>
+              </div>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={data.user} />
+      </SidebarFooter>
+    </Sidebar>
+  )
+}
--- rr_frontend/app/routes/dashboard/layout.tsx
+++ rr_frontend/app/routes/dashboard/layout.tsx
@@ -1,30 +1,17 @@
 import { Outlet } from "react-router";
+import { AppSidebar } from "~/components/app-sidebar";
 import { ModeToggle } from "~/components/mode-toggle";
 import { Separator } from "~/components/ui/separator";
 import {
   SidebarProvider,
   SidebarInset,
   SidebarTrigger,
-  SidebarRail,
-  Sidebar,
-  SidebarHeader,
-  SidebarContent,
-  SidebarFooter
 } from "~/components/ui/sidebar";
+
 export default function DashboardLayout() {
   return (
     <SidebarProvider>
-      <Sidebar collapsible="icon">
-        <SidebarHeader>
-
-        </SidebarHeader>
-        <SidebarContent>
-        </SidebarContent>
-        <SidebarFooter>
-
-        </SidebarFooter>
-        <SidebarRail />
-      </Sidebar>
+      <AppSidebar />
       <SidebarInset className="max-w-8xl mx-auto">
         <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 justify-between">
           <div className="flex items-center gap-2 px-4">
